5612095fda801fbd684e91ff31f75048a575621e
Ana Arias <acarias@espol.edu.ec>
2013-02-07 10:12:29 -0500

    nuevas funciones y modulo main

--- mastermind/src/mastermind.hs
+++ mastermind/src/mastermind.hs
@@ -64,3 +64,61 @@ sort_NPG k s
 										d = (4,3)
 										list@[x,y,z,xs]=[ a, b, c, d]
 										in list
+
+
+mastermind :: [(Int,Int)] -> IO()
+mastermind code = do
+	guess <- getLine
+	respond (sc_black ((converter guess)) (code)) (sc_white (coinc) ( sc_black ((converter guess)) (code)) ((converter guess) ) (code)) 1
+
+-- pegs black -> pegs white -> opportunitties
+respond :: Int -> Int -> Int -> IO ()
+respond guess n _| guess < n = do
+    putStrLn "Too low!"
+    mastermind n
+respond guess n _| guess > n = do
+    putStrLn "Too high!"
+    mastermind n
+respond _ _ _ | otherwise = putStrLn "Just right!"
+			
+guess:: IO() = do
+	linea <- getLine
+			
+converter :: String -> [(Int,Int)]
+converter [] = []
+converter (x:xs) = let
+					y = getInt x
+					a = (y,0)
+					in [a] ++ converter xs
+
+getInt:: Char -> Int
+getInt c
+	| c == '1' = 1
+	| c == '2' = 2
+	| c == '3' = 3
+	| c == '4' = 4
+	| c == '5' = 5
+	| c == '6' = 6
+					
+main = do
+		let 
+			pool = [1,2,3,4,5,6]
+			poblacion = [ [(a,0), (b,1), (c,2), (d,3) ] | a<-pool, b<-pool, c<-pool, d<-pool ]
+		print $ show(length(poblacion));
+		target <- randomRIO(0,200)
+		let code = poblacion !! target
+		
+		let 
+			black = sc_black [(1,0),(5,1),(3,2),(6,3)] [(1,0),(5,1),(3,2),(6,3)]
+			white = sc_white coinc black [(1,0),(5,1),(3,2),(6,3)] [(1,0),(5,1),(3,2),(6,3)]
+			sc = score black white
+			l_keep = keep 3 [1,0,2,3] [(1,0),(5,1),(3,2),(6,3)]
+			npg = n_pot_guess [1,2] [(1,0),(2,1),(3,2),(3,3)]
+		print code
+		print npg
+		print $ show( black );
+		print $ show( white );
+		
+		linea <- getLine
+		let adiv = converter linea
+		print adiv
